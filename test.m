7081679b1a847ab39879273701361b5712b8d4ae
hokolomopo <adrien.minne@outlook.com>
2018-10-14 15:20:26 +0200

    Add open loop simulation

--- test.m
+++ test.m
@@ -6,22 +6,39 @@ c = 2;
 % B = [0 0; 1/m 0];
 % C = [-1 0];
 % D = [0 1];
+% 
+% t = 0:0.01:100;
+% u = zeros(size(t, 2), 2);
+% x0 = [0.01 0];
 
 A = [0 1; -k/m -c/m];
 B = [0; 1/m];
 C = [1 0];
 D = [0];
 
-poles = eig(A);
-
-t = 0:0.01:100;
+step = 0.01;
+t = 0:step:100;
 u = zeros(size(t));
-x0 = [0.01 0];
+for i=1000:2000
+    u(i) = u(i) + sin(i*step);
+end
 
-sys = ss(A,B,C,0);
+x0 = [0 0];
 
+sys = ss(A,B,C,D);
+% step(sys)
+controlability_matrix = ctrb(sys);
+rank(controlability_matrix);
+
+figure
+subplot(2,1,1);
+plot(t, u);
+title('Input');
+
+subplot(2,1,2);
 [y,t,x] = lsim(sys,u,t,x0);
 plot(t,y)
 title('Open-Loop Response to Non-Zero Initial Condition')
-xlabel('Time (sec)')
 ylabel('Camera Position (m)')
+
+xlabel('Time (sec)')
